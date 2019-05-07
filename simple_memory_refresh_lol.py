6ad4ec26a197495d5ed8963d37fd802d11e132a2
lekdan49 <danindu19@gmail.com>
2019-05-07 19:03:57 +1000

    Create simple_memory_refresh_lol.py

---
+++ simple_memory_refresh_lol.py
@@ -0,0 +1,14 @@
+import os
+
+WebbSite = input("Please Enter the website or IP address you want me to ping! ")
+
+
+class DannMann:
+    fagbird = os.system("ping {}".format(WebbSite))
+
+
+def faggbird():
+    DannMann.fagbird
+
+
+faggbird()
